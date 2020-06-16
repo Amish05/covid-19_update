01805c8d410169113c93a8582dc9a8d611904c4f
Amish ali <63059501+Amish05@users.noreply.github.com>
2020-06-16 11:51:49 +0000

    Update covid-19_update v1.1.py

--- covid-19_update v1.1.py
+++ covid-19_update v1.1.py
@@ -1,28 +1,35 @@
 """
 how are you? i hope you all are well. in this program you will get corona virus latest update from
-WHO website. in this program i will use five python module
+WHO website. in this program i will use five python mode
 1.webbrowser
 2.time
 3.pyscreenshot
 4.os
 5.pyautogui
 """
+import webbrowser
+import pyscreenshot as ImageGrab     #pip install Pillow pyscreenshot
+import os
+import pyautogui # pip install pyautogui
+import os 
+import time
 def website():
     """
     this function is use to open website.
     """
-    import webbrowser
     url="https://covid19.who.int/region/emro/country/"+ country
+    webbrowser.open(url)
+def global_update():
+    url="https://covid19.who.int"
     webbrowser.open(url)
 def due_to_internet():
     """" 
     This is due to the speed of internet in Asia the speed of internet 
     is slow.
     """
-    import time
     time.sleep(10) #code Sleep for 10 seconds
 def screenshot():
-   """
+    """
     This function is used to save the screenshot
     """
  # for taking screenshot
@@ -30,16 +37,17 @@ def screenshot():
     filename=f"covid-19 {country} {localtime}.png"
     im = ImageGrab.grab(backend="mss", childprocess=False)    
     im.save(filename)
+
 def scroll_down():
     """
     This function is use to scroll down the wed page.
     """
-    # pip install pyautogui
-    import pyautogui # used for scrol down the web page
+ # used for scrol down the web page
     pyautogui.press('down',presses=1,interval=0.1)
 def get_country():
-    ex_country="Enter your country shortcuts for search your country\nlike pk for Pakistan,us for United Sate\ngb for The United Kingdom,in for India etc:"
+    ex_country="Enter your country shortcuts for search your country\nlike pk for Pakistan,us for United Sate\ngb for The United Kingdom,in for India etc"
     print(ex_country)
+    print(" AND for getting global updates enter g:")
     global country
     country=input()
     country=country.lower()
@@ -47,7 +55,11 @@ more_search="y"
 while(True):
     if(more_search=="y"):
         get_country()
-        website()
+        if (country=="g"):
+            global_update()
+            country="Global"
+        else:
+            website()
         country=country.upper()
         due_to_internet()
         scroll_down()
